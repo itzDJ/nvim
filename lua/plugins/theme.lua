29120f6c39f500be7524d84f4371830a33c65cfb
DJ <djcerami@proton.me>
2025-07-11 16:01:14 -0400

    readded transparent option

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,10 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        -- If `echo $COLORTERM` returns 'truecolor\n' then enable truecolor and the theme
-        if io.popen("echo $COLORTERM"):read("*a") == "truecolor\n" then
-            vim.opt.termguicolors = true
-            vim.cmd("colorscheme github_dark")
-        end
+        require("github-theme").setup({ options = { transparent = true } })
+        vim.cmd.colorscheme("github_dark")
     end,
 }
